359865a3b3ee9071a811518b12525e0e818cd55f
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-01-15 02:23:51 -0600

    fixed error

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -13,7 +13,7 @@ public class MySQLAdsDao implements Ads {
             DriverManager.registerDriver(new Driver());
             connection = DriverManager.getConnection(
                     config.getUrl(),
-                    config.getUsername(),
+                    config.getUser(),
                     config.getPassword()
             );
         } catch (SQLException e) {
